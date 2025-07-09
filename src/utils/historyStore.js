f4c8d5b82ab50b88ebfb9fec0b197004fc85d267
kurogamidesuu <hempushpchauhan@gmail.com>
2025-07-09 13:57:38 +0530

    added localStorage History

---
+++ src/utils/historyStore.js
@@ -0,0 +1,22 @@
+let history = null;
+let listeners = [];
+
+const initial_message = {
+  type: 'output',
+  text: 'Welcome to termiRoom\nWrite `help` to check the available commands\nVisit the Github repo for more information: [https://github.com/kurogamidesuu/TermiRoom.git]'
+}
+
+export const getLocalHistory = () => {
+  if(!history) {
+    const stored = localStorage.getItem('cmdHistory');
+    history = stored ? JSON.parse(stored) : [initial_message];
+    localStorage.setItem('cmdHistory', JSON.stringify(history));
+  }
+  return history;
+}
+
+export const setLocalHistory = (newHistory) => {
+  history = newHistory;
+  localStorage.setItem('cmdHistory', JSON.stringify(history));
+  listeners.forEach((cb) => cb(newHistory));
+}
